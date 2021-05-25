a744a8abd091c4ed6fcc03f6b7bd8bcfc1477d1b
jongwoniee <ljw3728@gmail.com>
2021-05-25 18:32:37 +0900

    front end for new and edit

--- app/reflexes/elements_reflex.rb
+++ app/reflexes/elements_reflex.rb
@@ -6,6 +6,8 @@ class ElementsReflex < ApplicationReflex
       element_record = Element.find(element['id'])
       element_record.update(position: element['position'])
     end
+    #Note might break with future stimulus reflex
+    @halted = true
   end
 
 end
