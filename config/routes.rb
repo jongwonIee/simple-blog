14ce0923cc2481b4a56e836adcaf645389320f53
jongwoniee <ljw3728@gmail.com>
2021-06-30 17:34:16 +0900

    delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
   devise_for :authors
-  # get 'home/index'
+  get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "readers/home#index"
-  get '/blog' => 'readers/home#index'
   get '/blog/:id' => 'readers/posts#show', as: :blog_post
 
   scope module: 'authors' do
+    get 'stats' => 'stats#index'
     resources :posts do
       resources :elements
     end
   end
-
 end
