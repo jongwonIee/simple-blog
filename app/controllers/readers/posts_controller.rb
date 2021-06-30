ac4161187bfd0547e4ff7cbf39e7e25b2611bde5
jongwoniee <ljw3728@gmail.com>
2021-07-01 05:05:35 +0900

    impressionist fix

--- app/controllers/readers/posts_controller.rb
+++ app/controllers/readers/posts_controller.rb
@@ -1,11 +1,14 @@
 module Readers
   class PostsController < ReadersController
-    impressionist :actions=>[:show]
+    before_action :set_post
     def show
-      @post = Post.published.friendly.find(params[:id])
+      impressionist(@post)
       @views = @post.impressionist_count
       @uniq_views = @post.impressionist_count(:filter=>:ip_address)
-      impressionist(@post)
+    end
+
+    def set_post
+      @post = Post.published.friendly.find(params[:id])
     end
   end
 end
