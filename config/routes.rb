080dc58c11bbaf7a6a4a525a55a3a483fe7b0389
jongwoniee <ljw3728@gmail.com>
2021-06-30 19:43:29 +0900

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :authors
-  get 'home/index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "readers/home#index"
   get '/blog/:id' => 'readers/posts#show', as: :blog_post
-
+  get '/blog' => "readers/home#index"
   scope module: 'authors' do
     get 'stats' => 'stats#index'
     resources :posts do
