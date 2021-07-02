b8bea76cb48d8f3bff5eb672e80b160db19ca7be
jongwoniee <ljw3728@gmail.com>
2021-07-02 16:20:37 +0900

    impressionist fix show views again

--- app/controllers/readers/posts_controller.rb
+++ app/controllers/readers/posts_controller.rb
@@ -5,9 +5,10 @@ module Readers
       begin
         impressionist(@post)
       rescue
+        puts('not recorded')
       end
-      # @views = @post.impressionist_count
-      # @uniq_views = @post.impressionist_count(:filter=>:ip_address)
+      @views = @post.impressionist_count
+      @uniq_views = @post.impressionist_count(:filter=>:ip_address)
     end
 
     def set_post
