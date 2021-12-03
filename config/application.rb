2a2eca54ff9cf3e20fd647b354f68c925ef81eba
jongwoniee <ljw3728@gmail.com>
2021-12-02 19:24:19 -0500

    shows unpublished on posts

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,5 @@ module SimpleBlog
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     #
-    config.action_dispatch.default_headers = {
-      'X-Frame-Options' => 'ALLOWALL'
-    }
   end
 end
